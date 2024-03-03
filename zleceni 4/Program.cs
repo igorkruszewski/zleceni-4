85cc260f2b9efaba45370aa8111416a455adef94
Igor <igorkris719@gmail.com>
2024-03-03 13:25:00 +0100

    jusz

--- zleceni 4/Program.cs
+++ zleceni 4/Program.cs
@@ -1,11 +1,12 @@
 ﻿const int cel = 100000;
 const int wwe = 5;
 int element = 1;
+int ilość = 1;
 for (element = 1; element <= cel; element++)
 {
     if (element %wwe == 0)
     {
         element = element++;
-        Console.WriteLine(element);
+        Console.WriteLine(ilość++ +" " + "element:"+ element);
     }
 }
