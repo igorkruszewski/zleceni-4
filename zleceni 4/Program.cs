fd1e742d4f7007b1b6de4f1159d43aff6648d6a0
Igor <igorkris719@gmail.com>
2024-03-03 13:31:30 +0100

    szczęście

--- zleceni 4/Program.cs
+++ zleceni 4/Program.cs
@@ -1,12 +1,12 @@
 ﻿const int cel = 100000;
 const int wwe = 5;
-int element = 1;
+//int element = 1;
 int ilość = 1;
-for (element = 1; element <= cel; element++)
+for (int element = 1; element <= cel; element++)
 {
     if (element %wwe == 0)
     {
-        element = element++;
+       // element = element++;
         Console.WriteLine(ilość++ +" " + "element:"+ element);
     }
 }
